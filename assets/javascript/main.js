e29456a42640d95d6a9ff3b7c921169177bace6c
Stephen Allen <staylorallen@gmail.com>
2019-08-02 14:03:43 -0400

    searchbar API functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,35 +1,35 @@
-document.addEventListener("DOMContentLoaded", function(func){
+document.addEventListener("DOMContentLoaded", function(func) {
 
 
-  function hearthstoneQuery(card){
+  function hearthstoneQuery(card) {
     fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + card, {
-      headers: {
-        "X-RapidAPI-Key": "de7e1386a0msh4a81fc191231dbbp121289jsn9b9280ac7bc4",
-      } // end headers          // **** DO NOT CHANGE API KEY ****
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Success");
-      console.log(data);
-    })
-    .catch(error => console.log(error))
-  };// end hearthstoneQuery function
+        headers: {
+          "X-RapidAPI-Key": "de7e1386a0msh4a81fc191231dbbp121289jsn9b9280ac7bc4",
+        } // end headers          // **** DO NOT CHANGE API KEY ****
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Success");
+        console.log(data);
+      })
+      .catch(error => console.log(error))
+  }; // end hearthstoneQuery function
 
 
-  function twitchQuery(){
+  function twitchQuery() {
     // hearthstoneID is the id from twitch.tv documentation.
     const hearthstoneID = "138585";
     fetch("https://api.twitch.tv/helix/streams?game_id=" + hearthstoneID, {
-      headers: {
-        "Client-ID": "liwyc586ihqciruhrzqtsu5o3vvs64",
-      }// end headers    // **** DO NOT CHANGE CLIENT ID KEY ****
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log("twitch Query success");
-      console.log(data);
-    })
-    .catch(error => console.log(error))
+        headers: {
+          "Client-ID": "liwyc586ihqciruhrzqtsu5o3vvs64",
+        } // end headers    // **** DO NOT CHANGE CLIENT ID KEY ****
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log("twitch Query success");
+        console.log(data);
+      })
+      .catch(error => console.log(error))
   }; // end TwitchQuery function
 
   hearthstoneQuery("Chicken");
